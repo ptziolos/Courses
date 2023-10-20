36a5311ca6bfcdede52d3e369f94987f256644fc
Philippos Tziolos <philippos.tziolos@scytalys.com>
2023-10-20 14:02:54 +0300

    10-react-quiz

---
+++ React/10-react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function FinishScreen({
+  maxPossiblePoints,
+  points,
+  highscore,
+  dispatch,
+}) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎊";
+  if (percentage >= 50 && percentage < 80) emoji = "🎉";
+  if (percentage >= 0 && percentage < 50) emoji = "👻";
+  if (percentage === 0) emoji = "🫠";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore})</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart
+      </button>
+    </>
+  );
+}
