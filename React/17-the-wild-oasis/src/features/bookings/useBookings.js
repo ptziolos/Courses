d1c338061cb7eee477ca9ec6da8f8b54d72d036f
Philippos Tziolos <philippos.tziolos@scytalys.com>
2025-04-29 10:48:58 +0300

    The wild Oasis

---
+++ React/17-the-wild-oasis/src/features/bookings/useBookings.js
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+
+export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  // : { field: "totalPrice", value: 5000, method: "gte" };
+  // for multiple filters an array can be used
+
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRow.split("-");
+  const sortBy = { field, direction };
+
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  const {
+    isLoading,
+    data: { data: bookings, count } = {},
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
+  });
+  return { isLoading, error, bookings, count };
+}
